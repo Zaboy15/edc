a5c6593426e7680d9cc307042756606ec2a73854
zakario <andrio.zaboy@gmail.com>
2019-10-05 04:23:28 +0700

    edit spk api

--- api/resources/spk.php
+++ api/resources/spk.php
@@ -10,9 +10,59 @@ switch ($request_method) {
         isAuthorizedApi("viewTickets");
 
         if(empty($filters)) {
-            $result = $database->select("spk", "*");
+            $result = $database->select("spk", [
+            "[><]clients" => ["id_merchant" => "id"]], [
+            "spk.id",
+            "spk.ticket_spk",
+            "spk.id_merchant",
+            "spk.spk_number",
+            "spk.inc_cimb",
+            "spk.id_itfs",
+            "spk.reported_time",
+            "spk.received_time",
+            "spk.wo_activity",
+            "spk.reason_code",
+            "spk.supply_kertas",
+            "spk.c_date_wo",
+            "spk.spk_status",
+            "spk.wo_remarks",
+            "spk.id_sn_edc",
+            "spk.id_sn_simcard",
+            "spk.test_credit",
+            "spk.test_prepaid",
+            "spk.test_debit",
+            "spk.sla_status",
+            "spk.root_cause",
+            "spk.sub_root_cause",
+            "spk.remarks_spk",
+            "clients.name"]);
         } else {
-            $result = $database->select("spk", "*", [ "AND" => $filters, "ORDER" => ["id" => "DESC"] ]);
+            $result = $database->select("spk", [
+            "[><]clients" => ["id_merchant" => "id"]], [
+            "spk.id",
+            "spk.ticket_spk",
+            "spk.id_merchant",
+            "spk.spk_number",
+            "spk.inc_cimb",
+            "spk.id_itfs",
+            "spk.reported_time",
+            "spk.received_time",
+            "spk.wo_activity",
+            "spk.reason_code",
+            "spk.supply_kertas",
+            "spk.c_date_wo",
+            "spk.spk_status",
+            "spk.wo_remarks",
+            "spk.id_sn_edc",
+            "spk.id_sn_simcard",
+            "spk.test_credit",
+            "spk.test_prepaid",
+            "spk.test_debit",
+            "spk.sla_status",
+            "spk.root_cause",
+            "spk.sub_root_cause",
+            "spk.remarks_spk",
+            "clients.name"], [ "AND" => $filters ]);
         }
 
         $i=0;
@@ -36,7 +86,7 @@ switch ($request_method) {
     case 'edit':
         isAuthorizedApi("editTicket");
 
-        $status = Spk::edit($data);
+        $status = Ticket::edit($data);
 
         if($status == 20) $response = [ "status" => 1, "status_message" => "Success! Item has been updated successfully." ];
         else $response = [ "status" => 2, "status_message" => "Error! Unable to update item." ];
