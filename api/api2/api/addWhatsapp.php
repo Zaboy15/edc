a638aaf9239c0fe91a69c126486f9bd3f04d1a2a
Zakario <andrio.zaboy@gmail.com>
2020-06-10 22:36:20 +0700

    ok

--- api/api2/api/addWhatsapp.php
+++ api/api2/api/addWhatsapp.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD']=="POST"){
         $insert = "INSERT INTO whatsapp VALUE(NULL,'$app','$sender','$message',NULL)";
     if (mysqli_query($con,$insert)) {
         # code...
-        $response['reply']= "Terima Kasih atas informasi nya .";
+        $response['reply']= "Terima kasih sudah me response. Mohon maaf atas keterbatasan kami dalam membalas satu persatu memerlukan waktu. Mohon kesediannya menunggu ya kak.";
         echo json_encode($response);
     } else {
         # code...
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD']=="POST"){
         echo json_encode($response);
 
     }
-    }
+}
     
 
     
