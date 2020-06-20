4e713b2e9138ff18686ae0e86f2f464ba590dad6
Zakario <andrio.zaboy@gmail.com>
2020-06-20 21:44:51 +0700

    ok

--- api/api2/api/addWhatsapp.php
+++ api/api2/api/addWhatsapp.php
@@ -2,17 +2,23 @@
 require "../config/connection.php";
 if ($_SERVER['REQUEST_METHOD']=="POST"){
     $response = array();
+    $responsetext = '';
+    
     $app = $_POST['app'];
     $sender = $_POST['sender'];
     $message = $_POST['message'];
-    $getrespon = mysqli_fetch_assoc(mysqli_query($con, "SELECT respon_text FROM whatsapp WHERE id = 2"));
-    $respon = $getrespon['respon_text'];
+
+    $sql = mysqli_query($con,"SELECT respon_text FROM whatsapp_field WHERE id = 2");
+    while ($a = mysqli_fetch_array($sql)){
+        $responsetext = $a['respon_text'];
+    }
+    
 
         # code...
         $insert = "INSERT INTO whatsapp VALUE(NULL,'$app','$sender','$message',NULL)";
     if (mysqli_query($con,$insert)) {
         # code...
-        $response['reply']= $respon;
+        $response['reply']= $responsetext;
         echo json_encode($response);
     } else {
         # code...
